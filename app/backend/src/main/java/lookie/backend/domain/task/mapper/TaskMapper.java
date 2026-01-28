fab469ee22a5302de61a40fb70ce5c4d6679d5da
KimJoonYi <wnsawol@naver.com>
2026-01-28 16:52:29 +0900

    feat : task 도메인 DB 접근 구조 초기화 및 mapper 테스트 추가

---
+++ app/backend/src/main/java/lookie/backend/domain/task/mapper/TaskMapper.java
@@ -0,0 +1,24 @@
+package lookie.backend.domain.task.mapper;
+
+import lookie.backend.domain.task.vo.TaskVO;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface TaskMapper {
+    void insert(TaskVO task);
+
+    TaskVO findById(Long taskId);
+
+    // [조회] 다음 할당할 작업 찾기
+    TaskVO findNextUnassignedForZoneForUpdate(@Param("zoneId") Long zoneId);
+
+    // [수정] 할당된 작업 상태 변경
+    int updateAssignToInProgress(
+            @Param("batchTaskId") Long batchTaskId,
+            @Param("workerId") Long workerId
+    );
+
+    // [수정] 작업 완료 상태 변경
+    int updateComplete(@Param("batchTaskId") Long batchTaskId);
+}
---
+++ app/backend/src/main/java/lookie/backend/domain/task/vo/TaskVO.java
@@ -0,0 +1,28 @@
+package lookie.backend.domain.task.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.apache.ibatis.type.Alias;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Alias("TaskVO")
+public class TaskVO {
+    private Long batchTaskId;
+    private Long batchId;
+    private Long zoneId;
+
+    private Long workerId; //nullable
+    private Long toteId;
+    private String status; //UNASSIGNED/IN_PROGRESS/COMPLETED
+
+    private LocalDateTime startedAt;
+    private LocalDateTime completedAt;
+
+}
