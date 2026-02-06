244ab8887e31753df7a4044c02d8a6696cdd44fa
yizyeo <xxikezyz42@naver.com>
2026-02-06 13:43:44 +0900

    fix: webRTC 관리자 간 연결 오류 해결

--- app/frontend/src/components/layout/AdminLayout.tsx
+++ app/frontend/src/components/layout/AdminLayout.tsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import AdminSidebar from "./AdminSidebar";
 import { useCallStore } from "@/stores/callStore";
-import VideoCallModal from "@/components/webrtc/VideoCallModal";
+
 
 export default function AdminLayout() {
   const listenForIncomingCalls = useCallStore((state) => state.listenForIncomingCalls);
@@ -31,8 +31,6 @@ export default function AdminLayout() {
         </div>
       </div>
 
-      {/* Global Call Modal */}
-      <VideoCallModal />
     </div>
   );
 }
